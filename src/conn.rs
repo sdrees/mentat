436415079232d7958319c8ca3c5bd491ac5e21bc
Grisha Kruglov <gkruglov@mozilla.com>
2018-07-25 15:28:48 -0700

    Pre: Split a Db error for clarity
    error_chain stack limitations no longer apply, so let's have better errors!

--- src/conn.rs
+++ src/conn.rs
@@ -889,7 +889,7 @@ mod tests {
 
         match conn.transact(&mut sqlite, t.as_str()) {
             Err(MentatError::DbError(e)) => {
-                assert_eq!(e.kind(), ::mentat_db::DbErrorKind::UnrecognizedEntid(next + 1));
+                assert_eq!(e.kind(), ::mentat_db::DbErrorKind::UnallocatedEntid(next + 1));
             },
             x => panic!("expected db error, got {:?}", x),
         }
@@ -917,7 +917,7 @@ mod tests {
         match conn.transact(&mut sqlite, t.as_str()) {
             Err(MentatError::DbError(e)) => {
                 // All this, despite this being the ID we were about to allocate!
-                assert_eq!(e.kind(), ::mentat_db::DbErrorKind::UnrecognizedEntid(next));
+                assert_eq!(e.kind(), ::mentat_db::DbErrorKind::UnallocatedEntid(next));
             },
             x => panic!("expected db error, got {:?}", x),
         }
